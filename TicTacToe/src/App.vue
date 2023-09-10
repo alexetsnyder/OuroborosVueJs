b0ee4a07b130183b237befd193b78b2ed9b06f51
Alex E.T. Snyder <alexetsnyder@gmail.com>
2023-09-10 14:43:09 -0400

    Update App.vue
    Look ahead that mostly always wins, or ties, unless you can make a move that wins in two ways.

--- TicTacToe/src/App.vue
+++ TicTacToe/src/App.vue
@@ -9,7 +9,7 @@ const board = ref([
 	['', '', '']
 ])
 
-function CheckForWinner(flatBoard:Array<string>) {
+function CheckForWinner(flatBoard:string[]) {
 	const checks = [
 		[0, 1, 2],
 		[3, 4, 5],
@@ -49,7 +49,9 @@ function MakeMove(x:number, y:number) {
 
 		player.value = 'O'
 
-		ComputerMove();
+        if (GetPossibleMoves(board.value).length > 0) {
+            ComputerMove();
+        }	
 	}
 }
 
@@ -60,20 +62,23 @@ function ComputerMove() {
 			return
 		}
 
-		let [i, j] = GetComputerMove()
+		let move = GetComputerMove()
 
-        if (i === -1 && j === -1) {
-            return
+        if (move) {
+            let [i, j] = move
+
+            board.value[i][j] = player.value;
+
+            player.value = 'X'
         }
-
-		board.value[i][j] = player.value;
-
-	    player.value = 'X'
 	}
 }
 
-function GetPossibleMoves(array:Array<Array<string>>) {
+function GetComputerMove() {
+    return Evaluate(board.value, player.value)
+}
 
+function GetPossibleMoves(array:string[][]) {
     let possibleMoves = []
 
     for (let i = 0; i < array.length; i++)
@@ -89,16 +94,16 @@ function GetPossibleMoves(array:Array<Array<string>>) {
     return possibleMoves
 }
 
-function GetArrayCopies(array:Array<Array<string>>, count:number)
+function GetArrayCopies(array:string[][], count:number)
 {
     const arrayCopies = []
     for (let i = 0; i < count; i++)
     {
         let arrayCopy = [
-		    ['', '', ''],
-		    ['', '', ''],
-		    ['', '', '']
-	    ]
+            ['', '', ''],
+            ['', '', ''],
+            ['', '', '']
+        ]
 
         for (let i = 0; i < array.length; i++) {
             for (let j = 0; j < array[i].length; j++) {
@@ -111,48 +116,142 @@ function GetArrayCopies(array:Array<Array<string>>, count:number)
     return arrayCopies
 }
 
-function GetComputerMove() {
-    let ret = EvaluateRecursive(board.value, player.value)
-    if (ret) {
-        return ret
-    }
-    return [-1, -1]
+interface Branch  {
+    moves: number[][];
+    winner: string | null;
 }
 
-function EvaluateRecursive(array:Array<Array<string>>, fPlayer:string) {
+interface Move {
+    move: number[];
+    wins: number;
+    loses: number;
+    ties: number;
+}
+
+function CreateBranch(moves:number[][], winner:string) : Branch {
+    return { moves: moves, winner: winner }
+}
+
+function Evaluate(array:string[][], fPlayer:string) : number[] {
     const possibleMoves = GetPossibleMoves(array)
 
     const arrayCopies = GetArrayCopies(array, possibleMoves.length)
 
-    const branches = []
+    const branches : Branch[] = []
     for (let i = 0; i < possibleMoves.length; i++) {
         if (EvaluateMove(arrayCopies[i], possibleMoves[i], fPlayer)) {
-            return (fPlayer === 'O') ? possibleMoves[i] : null
+            return possibleMoves[i]
         }
         else {
-            let branch : {
-                move: Array<number>;
-                game: Array<number> | null;
-            }
-
-            branch = {
-                move: possibleMoves[i],
-                game: EvaluateRecursive(arrayCopies[i], (fPlayer === 'X') ? 'O' : 'X')
-            }
-            branches.push(branch)
+            let possibleBranch = CreateBranch([possibleMoves[i]], '');
+            let bs = EvaluateRecursive(possibleBranch, arrayCopies[i], 'X')
+            branches.push(...bs)
         }
     }
 
-    for (let i = 0; i < branches.length; i++) {
-        if (branches[i].game) {
-            return branches[i].move
-        }       
-    }
+    const moves : Move[] = EvaluateBranches(branches)
 
-    return (branches.length > 0) ? branches[0].move : null
+    CullMoves(moves)
+
+    let maxChance = 0
+    let maxMove : Move = moves[0]
+    for (let i = 0; i < moves.length; i++) {
+        let move = moves[i]
+        let chance = move.wins / move.loses
+        if (chance > maxChance) {
+            maxChance = chance
+            maxMove = move
+        }
+    }
+    
+    return maxMove.move
 }
 
-function EvaluateMove(array:Array<Array<string>>, move:Array<number>, fPlayer:string) {
+function CullMoves(moves:Move[]) {
+    const removeArray = []
+    for (let i = 0; i < moves.length; i++) {
+        let move = moves[i]
+        if (move.wins === 0 && move.ties === 0 && move.loses > 0) {
+            removeArray.push(move)
+        }
+    }
+
+    if (removeArray.length < moves.length) {
+        for (let i = 0; i < removeArray.length; i++) {
+            let index = moves.indexOf(removeArray[i])
+            moves.splice(index, 1)
+        }
+    } 
+}
+
+function EvaluateBranches(branches:Branch[]) : Move[] {
+    const moves : Move[] = []
+    for (let i = 0; i < branches.length; i++) {
+        let branch = branches[i]
+        let isFound = false
+        for (let j = 0; j < moves.length; j++) {
+            let move = moves[j]
+            if (move.move === branch.moves[0]) {
+                EvaluateBranch(branch, move)
+                isFound = true;
+                break;
+            }
+        }
+
+        if (!isFound) {
+            let move : Move = {
+                move: branch.moves[0],
+                wins: 0,
+                loses: 0,
+                ties: 0,
+            }
+            EvaluateBranch(branch, move)
+            moves.push(move)
+        }
+    }
+    
+    return moves
+}
+
+function EvaluateBranch(branch:Branch, move:Move) {
+    if (branch.winner === 'O') {
+        move.wins++
+    }
+    else if (branch.winner === 'X') {
+        move.loses++
+    }
+    else {
+        move.ties++
+    }
+}
+
+function EvaluateRecursive(possibleBranch: Branch, array:string[][], fPlayer:string) : Branch[] {
+    const possibleMoves = GetPossibleMoves(array)
+
+    if (possibleMoves.length == 0) {
+        return [possibleBranch]
+    }
+
+    const arrayCopies = GetArrayCopies(array, possibleMoves.length)
+
+    const branches : Branch[] = []
+    for (let i = 0; i < possibleMoves.length; i++) {
+        if (EvaluateMove(arrayCopies[i], possibleMoves[i], fPlayer)) {
+            return [{ moves: possibleBranch.moves.concat([possibleMoves[i]]), winner: fPlayer }]
+        }
+        else {
+            let newPossibleBranch : Branch = {
+                moves: possibleBranch.moves.concat([possibleMoves[i]]),
+                winner: possibleBranch.winner
+            }
+            branches.push(...EvaluateRecursive(newPossibleBranch, arrayCopies[i], (fPlayer === 'X') ? 'O' : 'X'))
+        }
+    }
+
+    return branches
+}
+
+function EvaluateMove(array:string[][], move:number[], fPlayer:string) {
     let [i, j] = move
 
     array[i][j] = fPlayer
